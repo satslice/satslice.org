1841eaae3dfa98a39265777add434e19c00173c6
Scott Torborg <storborg@gmail.com>
2025-06-16 12:22:48 -0700

    Enable sitemap

--- site/.vitepress/config.mjs
+++ site/.vitepress/config.mjs
@@ -22,6 +22,10 @@ export default defineConfig({
 
   cleanUrls: true,
 
+  sitemap: {
+    hostname: 'https://www.satslice.org',
+  },
+
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
